761af1597adc7f22d9760262be57d7aa1c7fb9a3
Minindu Pansith <minindu.2017414@iit.ac.lk>
2020-04-13 01:48:48 +0530

    development: auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,23 @@
 const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    if(!token) {
+        res.status(401);
+        res.json({ msg: 'Authorization denied' });
+    } else {
+        try {
+            const decode = jwt.verify(token, keys.jwtSecret);
+            req.user = decode;
+            req.body.modifiedBy = decode.id;
+            next();
+        } catch (e) {
+            res.status(400);
+            res.json({ msg: 'invalid token' });
+        }
+    }
+}
+
+module.exports = auth;
