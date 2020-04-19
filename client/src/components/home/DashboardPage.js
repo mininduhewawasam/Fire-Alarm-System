c451996410d27ad0e3b7db61cd10f1ac162bba6d
pulze <malagodap2@gmail.com>
2020-04-19 17:11:31 +0530

    New changes...

--- client/src/components/home/DashboardPage.js
+++ client/src/components/home/DashboardPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchSensors } from "../../actionControllers/sensors";
+import { fetchSensors } from "../../actionControllers/Sensors";
 import { Card, Button, Image } from "semantic-ui-react";
+import Inactive from './Inactive.png';
+import Active from './active.jpg';
 
 class DashboardPage extends React.Component {
   state = {
@@ -15,52 +17,50 @@ class DashboardPage extends React.Component {
   render() {
     console.log(this.props.sensors);
     return (
-      <div>
+      <div className="page-wrapper">
+      <Card.Group itemsPerRow={4}>
         {this.props.sensors &&
           this.props.sensors.map((sensor) => {
             return (
-              <div>
-                <Card.Group>
-                  <Card>
-                    <Card.Content>
-                      {(sensor.status && sensor.status === 1) ? (
-                        <Image
-                          floated="right"
-                          size="mini"
-                          src="/active.jpg"
-                        />
-                      ) : (
-                        <Image
-                          floated="right"
-                          size="mini"
-                          src="/Inactive.png"
-                        />
-                      )}
-                      <Card.Header>{sensor.sensorName ? sensor.sensorName : "N/A"}</Card.Header>
-                      <Card.Description>
-                        Sensor Status : <strong>{sensor.status ? sensor.status : "N/A"}</strong><br/>
-                        Room Number   : <strong>{sensor.roomNo ? sensor.roomNo : "N/A"}</strong><br/>
-                        Room Number   : <strong>{sensor.floorNo ? sensor.floorNo : "N/A"}</strong><br/>
-                        location Name : <strong>{sensor.locationName ? sensor.locationName : "N/A"}</strong><br/>
-                        Co2 Level     : <strong>{sensor.co2Value ? sensor.co2Value : "N/A"}</strong><br/>
-                        Smoke Level   : <strong>{sensor.smokeLevel ? sensor.smokeLevel : "N/A"}</strong><br/>
-                      </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <div className="ui two buttons">
-                        <Button basic color="green">
-                          Edit
-                        </Button>
-                        <Button basic color="red">
-                          Delete
-                        </Button>
-                      </div>
-                    </Card.Content>
-                  </Card>
-                </Card.Group>
-              </div>
+                <Card>
+                  <Card.Content>
+                    {(sensor.status && sensor.status === 1) ? (
+                      <Image
+                        floated="right"
+                        size="mini"
+                        src={Active}
+                      />
+                    ) : (
+                      <Image
+                        floated="right"
+                        size="mini"
+                        src={Inactive}
+                      />
+                    )}
+                    <Card.Header>{sensor.sensorName ? sensor.sensorName : "N/A"}</Card.Header>
+                    <Card.Description>
+                      Sensor Status : <strong>{sensor.status ? sensor.status : "N/A"}</strong><br/>
+                      Room Number   : <strong>{sensor.roomNo ? sensor.roomNo : "N/A"}</strong><br/>
+                      Room Number   : <strong>{sensor.floorNo ? sensor.floorNo : "N/A"}</strong><br/>
+                      location Name : <strong>{sensor.locationName ? sensor.locationName : "N/A"}</strong><br/>
+                      Co2 Level     : <strong>{sensor.co2Value ? sensor.co2Value : "N/A"}</strong><br/>
+                      Smoke Level   : <strong>{sensor.smokeLevel ? sensor.smokeLevel : "N/A"}</strong><br/>
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <div className="ui two buttons">
+                      <Button basic color="green">
+                        Edit
+                      </Button>
+                      <Button basic color="red">
+                        Delete
+                      </Button>
+                    </div>
+                  </Card.Content>
+                </Card>
             );
           })}
+      </Card.Group>
       </div>
     );
   }
