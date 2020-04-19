21edcb16201ba16e571619f48c219da3290c7627
HarendraRanasinghe95 <harendra.ranasinghe@itelasoft.com>
2020-04-19 17:55:25 +0530

    delete sensor

--- client/src/components/home/DashboardPage.js
+++ client/src/components/home/DashboardPage.js
@@ -1,19 +1,44 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import DeleteModal from "./DeleteModal";
 import { fetchSensors } from "../../actionControllers/sensors";
 import { Card, Button, Image } from "semantic-ui-react";
 
 class DashboardPage extends React.Component {
   state = {
-    errors: {},
+    editSensorModalState: false,
+    removeSensorModalState: false,
   };
 
-  componentDidMount = () => this.onInit(this.props);
-  onInit = (props) => props.fetchSensors();
+  componentDidMount = () => this.props.fetchSensors();
+
+  handleRemoveSensorModalState(state) {
+    this.setState({ removeSensorModalState: state });
+  }
+
+  handleEditSensorModalState(state) {
+    this.setState({ editSensorModalState: state });
+  }
+
+  removeSensor = (selectedSensor) => {
+    const data = {
+      sensorId: selectedSensor.sensorId,
+      roomId: selectedSensor.floorNo,
+      ownerId: selectedSensor.ownerId,
+      locationId: selectedSensor.locationId,
+      floorId: selectedSensor.floorId,
+      name: selectedSensor.sensorName,
+    };
+    axios
+      .get(`/api/sensor`, { data })
+      .then((res) => res.data.pages)
+      .then(this.handleRemoveSensorModalState(false))
+      .then(this.props.location.reload());
+  };
 
   render() {
-    console.log(this.props.sensors);
     return (
       <div>
         {this.props.sensors &&
@@ -23,12 +48,8 @@ class DashboardPage extends React.Component {
                 <Card.Group>
                   <Card>
                     <Card.Content>
-                      {(sensor.status && sensor.status === 1) ? (
-                        <Image
-                          floated="right"
-                          size="mini"
-                          src="/active.jpg"
-                        />
+                      {sensor.status && sensor.status === 1 ? (
+                        <Image floated="right" size="mini" src="/active.jpg" />
                       ) : (
                         <Image
                           floated="right"
@@ -36,14 +57,36 @@ class DashboardPage extends React.Component {
                           src="/Inactive.png"
                         />
                       )}
-                      <Card.Header>{sensor.sensorName ? sensor.sensorName : "N/A"}</Card.Header>
+                      <Card.Header>
+                        {sensor.sensorName ? sensor.sensorName : "N/A"}
+                      </Card.Header>
                       <Card.Description>
-                        Sensor Status : <strong>{sensor.status ? sensor.status : "N/A"}</strong><br/>
-                        Room Number   : <strong>{sensor.roomNo ? sensor.roomNo : "N/A"}</strong><br/>
-                        Room Number   : <strong>{sensor.floorNo ? sensor.floorNo : "N/A"}</strong><br/>
-                        location Name : <strong>{sensor.locationName ? sensor.locationName : "N/A"}</strong><br/>
-                        Co2 Level     : <strong>{sensor.co2Value ? sensor.co2Value : "N/A"}</strong><br/>
-                        Smoke Level   : <strong>{sensor.smokeLevel ? sensor.smokeLevel : "N/A"}</strong><br/>
+                        Sensor Status :{" "}
+                        <strong>{sensor.status ? sensor.status : "N/A"}</strong>
+                        <br />
+                        Room Number :{" "}
+                        <strong>{sensor.roomNo ? sensor.roomNo : "N/A"}</strong>
+                        <br />
+                        Floor Number :{" "}
+                        <strong>
+                          {sensor.floorNo ? sensor.floorNo : "N/A"}
+                        </strong>
+                        <br />
+                        location Name :{" "}
+                        <strong>
+                          {sensor.locationName ? sensor.locationName : "N/A"}
+                        </strong>
+                        <br />
+                        Co2 Level :{" "}
+                        <strong>
+                          {sensor.co2Value ? sensor.co2Value : "N/A"}
+                        </strong>
+                        <br />
+                        Smoke Level :{" "}
+                        <strong>
+                          {sensor.smokeLevel ? sensor.smokeLevel : "N/A"}
+                        </strong>
+                        <br />
                       </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
@@ -51,7 +94,11 @@ class DashboardPage extends React.Component {
                         <Button basic color="green">
                           Edit
                         </Button>
-                        <Button basic color="red">
+                        <Button
+                          basic
+                          color="red"
+                          onClick={this.handleRemoveSensorModalState(true)}
+                        >
                           Delete
                         </Button>
                       </div>
@@ -61,12 +108,22 @@ class DashboardPage extends React.Component {
               </div>
             );
           })}
+
+        <DeleteModal
+          isOpen={this.state.editDeviceModalState}
+          hideModal={() => this.handleEditDeviceModalState(false, {})}
+          device={this.state.selectedDevice}
+          saveDetails={this.updateDevice}
+        />
       </div>
     );
   }
 }
 
 DashboardPage.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
   fetchSensors: PropTypes.func.isRequired,
   sensors: PropTypes.arrayOf(
     PropTypes.shape({
